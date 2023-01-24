2114daa8c4013b4d924b261044471242e9a01f58
loyd_ <loyd_pintac@yahoo.com>
2023-01-24 14:45:00 -0600

    Adding model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,3 @@
 class Pet < ApplicationRecord
+  validates :Breed, length: {minimum: 3}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_185848) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+
     t.datetime "created_at"
     t.datetime "updated_at"
   end
